12ddaf1133597bb7299ac88912f6c4a394c1a0db
YunSukHyun <czcz9207@gmail.com>
2023-11-19 02:28:21 +0900

    webpack image load

--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
 import generateJoke from "./generateJoke";
 import "./styles/main.scss";
+import sad from "./assets/sad.png";
+
+const sadImg = document.getElementById("sadImg");
+sadImg.src = sad;
 console.log(generateJoke());
-console.log(1);
--- webpack.config.js
+++ webpack.config.js
@@ -9,12 +9,15 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "[name][contenthash].js",
+    clean: true,
+    assetModuleFilename: "[name][ext]",
   },
+  devtool: "source-map",
   devServer: {
     static: {
       directory: path.resolve(__dirname, "dist"),
     },
-    devtool: "source-map",
+
     port: 3000,
     open: true,
     hot: true,
@@ -24,6 +27,18 @@ module.exports = {
   module: {
     rules: [
       { test: /\.scss$/, use: ["style-loader", "css-loader", "sass-loader"] },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: { presets: ["@babel/preset-env"] },
+        },
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/i,
+        type: "asset/resource",
+      },
     ],
   },
   plugins: [
