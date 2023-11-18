3a4110a519a8d0a3a6f379a903c08af6c25aed51
YunSukHyun <czcz9207@gmail.com>
2023-11-19 00:15:13 +0900

    webpack with module

---
+++ webpack.config.js
@@ -0,0 +1,17 @@
+const path = require("path");
+
+module.exports = {
+  mode: "development",
+  entry: {
+    bundle: path.resolve(__dirname, "src/index.js"),
+  },
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "[name].js",
+  },
+  module: {
+    rules: [
+      { test: /\.scss$/, use: ["style-loader", "css-loader", "sass-loader"] },
+    ],
+  },
+};
