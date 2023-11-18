f26ea77886d120e510feed5b5fe897f333ada4c2
YunSukHyun <czcz9207@gmail.com>
2023-11-19 02:38:19 +0900

    add function

--- src/index.js
+++ src/index.js
@@ -4,4 +4,7 @@ import sad from "./assets/sad.png";
 
 const sadImg = document.getElementById("sadImg");
 sadImg.src = sad;
+
+const jokeBtn = document.getElementById("jokeBtn");
+jokeBtn.addEventListener("click", generateJoke);
 console.log(generateJoke());
